cbc7d79e4c1d2e59769108606214a5bc6a80ca83
Harry Chou <yarram1997@gmail.com>
2019-07-22 15:51:18 +0800

    按之字形顺序打印二叉树

--- problems/树/对称的二叉树/SymmetricalTree.java
+++ problems/树/对称的二叉树/SymmetricalTree.java
@@ -1,44 +1,46 @@
 /**
  * Created by Harry Chou at 2019/7/22.
  */
+import java.util.Stack;
+
 public class SymmetricalTree {
-    boolean isSymmetrical(TreeNode pRoot){
-        if(pRoot==null){
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
             return true;
         }
-        return recur(pRoot.left,pRoot.right);
-    }
-    
-    private boolean recur(TreeNode left,TreeNode right){
-        if(left==null){
-            return right==null;
-        }
-        if(right==null){
-            return false;
-        }
-        if(left.val!=right.val){
-            return false;
-        }
-        return recur(left.left,right.right)&&recur(left.right,right.left);
+        return recur(pRoot.left, pRoot.right);
     }
 
-     boolean isSymmetricalWithoutRecur(TreeNode pRoot){
-        if(pRoot==null){
+    private boolean recur(TreeNode left, TreeNode right) {
+        if (left == null) {
+            return right == null;
+        }
+        if (right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return recur(left.left, right.right) && recur(left.right, right.left);
+    }
+
+    boolean isSymmetricalWithoutRecur(TreeNode pRoot) {
+        if (pRoot == null) {
             return true;
         }
         Stack<TreeNode> stack = new Stack<>();
         stack.push(pRoot.left);
         stack.push(pRoot.right);
-        while(!stack.empty()){
+        while (!stack.empty()) {
             TreeNode right = stack.pop();
             TreeNode left = stack.pop();
-            if(left==null && right==null){
+            if (left == null && right == null) {
                 continue;
             }
-            if(left==null || right==null){
+            if (left == null || right == null) {
                 return false;
             }
-            if(left.val!=right.val){
+            if (left.val != right.val) {
                 return false;
             }
             stack.push(left.left);
